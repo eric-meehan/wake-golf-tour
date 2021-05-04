3748115ca040434d2b2fc98007e2e2b032bd6a81
Eric Meehan <eric.meehan@homodeus.us>
2021-05-04 13:30:08 -0400

    abc

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ from pkg_resources import Requirement
 
 pkg_name = 'eomwgtsite'
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/env/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-
 site_path = working_set.find(Requirement.parse(pkg_name)).location + "/" + '\\WGT_Website\\'
 
 command = 'python3 ' + site_path + '/manage.py runserver'
+os.system(command)
+command = 'source ' + site_path + '/env/bin/activate'
 os.system(command)
 
 #url = 'http://localhost:8000/'
