c82b3e2f62f430e7f37ab2a29e43357ed7943ebb
Eric Meehan <eric.meehan@homodeus.us>
2021-05-04 13:34:59 -0400

    abc

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ pkg_name = 'eomwgtsite'
 
 site_path = working_set.find(Requirement.parse(pkg_name)).location + "/" + '\\WGT_Website\\'
 
-command = 'python3 ' + site_path + '/manage.py runserver'
-os.system(command)
 command = 'source ' + working_set.find(Requirement.parse(pkg_name)).location + '/env/bin/activate'
+os.system(command)
+
+command = 'python3 ' + site_path + '/manage.py runserver'
 os.system(command)
 
 #url = 'http://localhost:8000/'
